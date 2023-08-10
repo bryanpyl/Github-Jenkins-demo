31b54fb13411dbdd88871235b8d4cc6ba8c7e47b
Bryan Phang <105845570+bryanpyl@users.noreply.github.com>
2023-08-10 16:23:42 +0800

    Updated 10/8/2023

---
+++ demo.py
@@ -0,0 +1,11 @@
+import os
+List1 = [0, 2, 4, 6, 8, 10]
+List1.reverse()
+print("The reverse list is:",List1)
+
+List2 = [45, 987, 102, 63, 45, 562, 102, 1, 19, 55, 300]
+List2.sort()
+print ("The sorted list is:", List2)
+
+indexvalue = List2[2:6]
+print("The index value are:", indexvalue)
